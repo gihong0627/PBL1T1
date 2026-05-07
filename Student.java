15f23d442923852fe7253c7c4cb415e39edef8cb
gihong00627 <gihonglim0627@gmail.com>
2026-05-07 17:29:18 +0900

    프로젝트 공유 초기화

---
+++ Student.java
@@ -0,0 +1,88 @@
+
+/**
+ * Student 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class Student
+{
+    private String name;
+    private String st_id;
+    private int midTest;     
+    private int finalTest;   
+    private int assignment; 
+    private int attendance; 
+    private double totalScore; 
+    private String grade;
+
+    /**
+     * Student 클래스의 객체 생성자
+     */
+    public Student(String name, String st_id,
+                   int midTest, int finalTest,
+                   int assignment, int attendance)
+    {
+        this.name = name;
+        this.st_id = st_id;
+        this.midTest = midTest;
+        this.finalTest = finalTest;
+        this.assignment = assignment;
+        this.attendance = attendance;
+        calcTotalScore(); 
+    }
+    
+    /**
+     * 메소드 calcTotalScore 총점수를 계산하는 메소드
+     *
+     */
+    public void calcTotalScore() {
+        this.totalScore = this.midTest * 0.35 + finalTest * 0.35
+                        + assignment + attendance;
+    }
+    
+    /**
+     * 메소드 setGrade 학점 등급을 부여하는 메소드
+     *
+     * @param grade 문자열로 이루어진 학점 등급
+     */
+    public void setGrade(String grade) {
+        this.grade = grade;
+    } 
+    
+    /**
+     * 메소드 getStId 학점을 반환하는 메소드
+     *
+     * @return 학점
+     */
+    public String getStId()  { 
+        return st_id; 
+    }
+    
+    /**
+     * 메소드 getName 이름을 반환하는 메소드
+     *
+     * @return 이름
+     */
+    public String getName()  { 
+        return name; 
+    }
+    
+    /**
+     * 메소드 getTotalScore 총점수를 반환하는 메소드
+     *
+     * @return 총점수
+     */
+    public double getTotalScore() { 
+        return totalScore; 
+    }
+    
+    /**
+     * 메소드 getGrade 학점 등급을 반환하는 메소드
+     *
+     * @return 학점 등급
+     */
+    public String getGrade() { 
+        return grade; 
+    }
+}
