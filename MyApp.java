ee831110b84468117d494948ba5dff82c1243a32
gihong00627 <gihonglim0627@gmail.com>
2026-05-08 12:47:45 +0900

    메소드 연결 제외한 기본적인 main 메소드 작성

--- MyApp.java
+++ MyApp.java
@@ -1,21 +1,61 @@
-
+import java.util.Scanner;
 /**
  * MyApp 성적 처리 소프트웨어 실행 클래스.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (2021320032 임기홍)
+ * @version (2026 05 08)
  */
 public class MyApp
 {
+    static Scanner gradeSC = new Scanner(System.in);
 
 
     /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 메소드 main
      *
-     * @param  y  메소드의 샘플 파라미터
+     * @param  args 파라미터
      * 
      */
     public static void main(String[] args){
         
+        Student[][] stdb = new Student[10][200];
+        int[] counts = new int[10];
+        
+        while(true){
+            
+            System.out.println("-----성적 처리 프로그램-----");
+            System.out.println("1.학생 등록 \t 2.학점 부여");
+            System.out.println("3.성적 출력 \t 4.종료");
+            System.out.println("-------------------------");
+            System.out.println("번호를 입력해주세요 : ");
+            
+            int choice;
+            try{
+                choice = Integer.parseInt(gradeSC.nextLine());
+            }
+            catch(NumberFormatException e){
+                System.out.println("숫자를 입력해주세요");
+                continue;
+            }
+            
+            switch(choice){
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    {
+                        System.out.println("프로그램을 종료합니다");
+                        System.exit(1);
+                    }
+                default:
+                    System.out.println("1~4 숫자만 선택해주세요");
+            }
+        }
     }
 }
