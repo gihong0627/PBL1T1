d2919144f84eeb9d5da55cc2e6d739e09263ad02
gihong0627 <gihonglim0627@gmail.com>
2026-05-13 11:47:26 +0900

    상대평가 C -> F로 수정

--- MyApp.java
+++ MyApp.java
@@ -224,7 +224,7 @@ public class MyApp
                 stdb[code][i].setGrade("B");
             }
             else{
-                stdb[code][i].setGrade("C");
+                stdb[code][i].setGrade("F");
             }
         }
     }
