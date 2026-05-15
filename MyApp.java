c14f79b0a498853617bc8d06482d98e6f03bf583
gihong00627 <gihonglim0627@gmail.com>
2026-05-15 13:26:18 +0900

    주석 수정

--- MyApp.java
+++ MyApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  * MyApp 성적 처리 소프트웨어 실행 클래스.
  *
  * @author (2021320032 임기홍 2020315044 허진영 2021320020 차인택)
- * @version (2026 05 14)
+ * @version (2026 05 15)
  */
 public class MyApp
 {
